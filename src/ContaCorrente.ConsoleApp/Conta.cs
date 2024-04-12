625a7dadcf4a655f40636d50b791c32d8d89a112
Fabis-li <fabiano_slima@hotmail.com>
2024-04-12 11:46:42 -0300

    movimentacao

--- src/ContaCorrente.ConsoleApp/Conta.cs
+++ src/ContaCorrente.ConsoleApp/Conta.cs
@@ -3,9 +3,10 @@
     public class Conta
     {
         public int numero;
-        public string nome;
+        public Cliente titular;
         public decimal saldo;
         public decimal limite;
+        public Movimentacao[] movimentacao;
 
         public void Saca(decimal quantidade)
         {
@@ -15,6 +16,39 @@
         public void Deposita(decimal quantidade)
         {
             this.saldo += quantidade;
+        }
+
+        public bool VerificarSaque(decimal valor)
+        {
+            if(this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Transferir(Conta destino, decimal valor)
+        {
+            this.saldo -= valor;
+            destino.saldo += valor; 
+        }
+
+        public bool VerificarTransferencia(Conta destino, decimal valor)
+        {
+            bool retirou = this.VerificarSaque(valor);
+            if(retirou == false)
+            {
+                return false;
+            }
+            else
+            {
+                destino.Deposita(valor);
+                return true;
+            }
         }
     }
 }
--- src/ContaCorrente.ConsoleApp/Program.cs
+++ src/ContaCorrente.ConsoleApp/Program.cs
@@ -5,7 +5,28 @@
         static void Main(string[] args)
         {
             Conta c1 = new Conta();
-            c1.nome = "Fabiano";
+            Cliente fabiano = new Cliente();
+
+            c1.titular.nome = "Fabiano";
+
+            c1.saldo = 1000;
+
+            c1.Saca(200);
+
+            c1.Deposita(500);
+
+            bool conseguiu = c1.VerificarSaque(2000);
+
+            if(conseguiu)
+            {
+                Console.WriteLine("Conseguiu sacar");
+            }
+            else
+            {
+                Console.WriteLine("Não conseguiu sacar");
+            }
+
+            Console.WriteLine(conseguiu);
         }
     }
 }
